32d02950af3a47592b26109f13bab48eb7d249a4
armena <armen.arzumanyan@optym.com>
2019-04-02 16:20:18 +0400

    next best push

---
+++ marshal/src/main/java/io/project/app/marshal/MarshalApplication.java
@@ -0,0 +1,13 @@
+package io.project.app.marshal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarshalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarshalApplication.class, args);
+	}
+
+}
