48588da99a445f6ddb4ff24510f04454a87e535d
Armen Arzumanyan <armena@YLT160015.CORP.OPTYM.NET>
2019-04-02 23:36:07 +0400

    next best push

--- marshal/src/main/java/io/project/app/repositories/QuestionRepository.java
+++ marshal/src/main/java/io/project/app/repositories/QuestionRepository.java
@@ -1,6 +1,6 @@
 package io.project.app.repositories;
 
-import org.aspectj.weaver.patterns.TypePatternQuestions.Question;
+import io.project.app.domain.Questions;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
  */
 @Component
 @Repository
-public interface QuestionRepository extends MongoRepository<Question, String> {
+public interface QuestionRepository extends MongoRepository<Questions, String> {
     
 
 }
