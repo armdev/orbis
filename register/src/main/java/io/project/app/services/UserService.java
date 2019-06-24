7ddde49079d2adbf91d894a77be344dd300a87b1
armena <armena@YLT160003.corp.optym.net>
2019-06-24 13:52:57 +0400

    update register

--- register/src/main/java/io/project/app/services/UserService.java
+++ register/src/main/java/io/project/app/services/UserService.java
@@ -4,6 +4,7 @@ import io.project.app.domain.User;
 import io.project.app.dto.UserData;
 import io.project.app.repositories.UserRepository;
 import io.project.app.util.CommonConstants;
+import io.project.app.util.PasswordHash;
 import java.util.List;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
@@ -32,6 +33,7 @@ public class UserService {
 
     public User save(User user) {
         user.setStatus(CommonConstants.ACTIVATED);
+        user.setPassword(PasswordHash.hashPassword(user.getPassword()));
         User savedUser = userRepository.save(user);
         UserData userData = new UserData(savedUser.getId(), savedUser.getFirstname(), savedUser.getLastname());
         
